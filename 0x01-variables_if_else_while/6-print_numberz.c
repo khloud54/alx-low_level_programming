ff655687f1e425a77dd4e4ba8eee3220bf386d7a
khloud tarek <kh.t705042000@gmail.com>
2023-08-11 12:10:47 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry boint
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
