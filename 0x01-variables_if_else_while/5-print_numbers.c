19a8affb4c0e21e6204f5ff246e1cf1477084d79
khloud tarek <kh.t705042000@gmail.com>
2023-08-11 12:01:23 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf("\n");
+	return (0);
+}
