efa5ac12e9f90b11f213316ee9dba1fd6b6eece7
khloud tarek <kh.t705042000@gmail.com>
2023-08-17 12:51:44 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - a function that prints the numbers
+ * Return: void
+*/
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == 2 || num == 4)
+			continue;
+		_putchar(num + 48);
+	}
+	_putchar('\n');
+}
