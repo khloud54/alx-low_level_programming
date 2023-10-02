00341c1bf14b258c159361e63e700d56a4de4f13
khloud54 <kh.t705042000@gmail.com>
2023-10-02 13:16:36 -0700

    0-read_textfile.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * rot13 - a function that encodes a string using rot13
- * @s: pointer to string
- * Return: void
+ * rot13 - encodes a string using rot13
+ * @s: input string
+ * Return: encoded string
 */
-
 char *rot13(char *s)
 {
 	int i;
-	int j;
-	char data1[] =
-	"ABCDEFGHIJKLMNOPRSTUVWXYZabcdesfghijklmnopqrstuvwxyz";
-	char datarot[] =
-	"NOPQRSTUVWXYZABCDEFGHIJKLMNopqrstuvwxyzabcdefghijklm";
-	for (i = 0; s[i] != '\0'; i++)
+
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	char Rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+	char *ptr = s;
+
+	while (*s)
 	{
-		for (j = 0; j < 52; j++)
+		for (i = 0; i <= 52; i++)
 		{
-			if (s[i] == data1[j])
+			if (*s == rot13[i])
 			{
-				s[i] = datarot[j];
+				*s = ROT13[i];
 				break;
 			}
 		}
+		s++;
 	}
+	return (ptr);
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# alx-low_level_programming
+1-create_file.c
