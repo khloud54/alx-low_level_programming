b101c29169ce27c9182a638c48cc4f66fb41f555
khloud54 <kh.t705042000@gmail.com>
2023-10-02 13:26:07 -0700

    0-read_textfile.c

--- 0-read_textfile.c
+++ 0-read_textfile.c
@@ -1,36 +1,25 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
+ * read_textfile - reads text from a file and prints it
+ * @filename: name of file to read
+ * @letters: number of bytes to read
  *
- * Return: numbers of letters printed. It fails, returns 0.
+ * Return: number bytes read/printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
 
-	if (!filename)
+	if (!filename || !letters)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
-
-	free(buf);
-
-	return (nwr);
+	return (bytes);
 }
